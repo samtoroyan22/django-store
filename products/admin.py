cf617a6029a15908db3a703fd4169d8eb0aaaa21
Тороян Самвел <toroyan_s@itstep.loc>
2025-06-03 20:04:46 +0300

    django admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 
 from products.models import Product, ProductCategory, Basket
 
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(ProductCategory)
 admin.site.register(Basket)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    list_filter = ("category",)
+    search_fields = ('name',)
+    ordering = ('name', 'price')
+    fields = ('name', ('price', 'quantity', 'category'), 'image', 'description', 'short_description')
+    readonly_fields = ('short_description', 'description')
+
+
+class BasketAdminInline(admin.TabularInline):
+    model = Basket
+    filter = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from users.models import User
+from products.admin import BasketAdminInline
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = (BasketAdminInline,)
+
+
